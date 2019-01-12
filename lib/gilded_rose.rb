23ee3a229df1baf5a53083824bdb7acda69a7301
mariusgg <marius.g.guei@gmail.com>
2019-01-12 17:58:45 +0000

    extracted normal items and aged brie

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,51 +4,93 @@ class GildedRose
     @items = items
   end
 
-  def update_quality
+
+  def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality -=  1
-          end
-        end
+      if item.name == "Aged Brie"
+        aged_brie(item)
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        backstage_passes(item)
+      elsif item.name == "Sulfuras, Hand of Ragnaros"
+        sulfuras(item)
+      elsif item.name == "Conjured"
+        conjured(item)
       else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in -= 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality -= 1
-              end
-            end
-          else
-            item.quality -= item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
+        normal_item(item)
       end
     end
   end
+
+  def normal_item(item)
+    if item.quality == 0
+      item.sell_in -= 1
+    else item.quality < 50
+      item.quality -= 1
+      item.sell_in -= 1
+      if item.sell_in <= 0
+        item.quality -= 1
+      end
+    end
+  end
+  def aged_brie(item)
+     if item.sell_in > 0
+       item.quality +=  1
+     else
+       item.quality += 2
+     end
+
+     item.sell_in -= 1
+     if item.quality > 50
+       item.quality = 50
+     end
+   end
 end
+  #
+  # def update_quality
+  #   @items.each do |item|
+  #     if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #       if item.quality > 0
+  #         if item.name != "Sulfuras, Hand of Ragnaros"
+  #           item.quality -=  1
+  #         end
+  #       end
+  #     else
+  #       if item.quality < 50
+  #         item.quality += 1
+  #         if item.name == "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.sell_in < 11
+  #             if item.quality < 50
+  #               item.quality += 1
+  #             end
+  #           end
+  #           if item.sell_in < 6
+  #             if item.quality < 50
+  #               item.quality += 1
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #     if item.name != "Sulfuras, Hand of Ragnaros"
+  #       item.sell_in -= 1
+  #     end
+  #     if item.sell_in < 0
+  #       if item.name != "Aged Brie"
+  #         if item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.quality > 0
+  #             if item.name != "Sulfuras, Hand of Ragnaros"
+  #               item.quality -= 1
+  #             end
+  #           end
+  #         else
+  #           item.quality -= item.quality
+  #         end
+  #       else
+  #         if item.quality < 50
+  #           item.quality += 1
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
+  #end
