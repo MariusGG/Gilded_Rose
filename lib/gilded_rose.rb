a7316cd54556acb1a6be6fe34a72804355e17d06
mariusgg <marius.g.guei@gmail.com>
2019-01-13 18:45:48 +0000

    updates with ternary operator

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -22,27 +22,20 @@ class GildedRose
   end
 
   def normal_item(item)
-    if item.quality == 0
-      item.sell_in -= 1
-    else item.quality < 50
-      item.quality -= 1
-      item.sell_in -= 1
-      item.quality = 0 if item.sell_in <= 0
-    end
+    item.quality -= 1 if item.quality > 0
+    item.quality -= 1 if item.sell_in <= 0 && item.quality > 0
+    item.sell_in -= 1
   end
 
   def aged_brie(item)
-    item.quality += if item.sell_in > 0
-      1
-    else
-      2
-    end
+    item.quality += 1 if item.quality < 50
+    item.quality += 1 if item.quality < 50 && item.sell_in <= 0
     item.sell_in -= 1
-    item.quality = 50 if item.quality > 50
   end
 
   # this seems to pass(2)test even though its empty
-  def sulfuras(item); end
+  def sulfuras(item);
+  end
 
   def backstage_passes(item)
     item.quality += 1
@@ -51,4 +44,5 @@ class GildedRose
     item.quality = 0 if item.sell_in <= 0
     item.quality = 50 if item.quality > 50
   end
+
 end
