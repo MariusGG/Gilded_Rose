b5026b94d4396def68261b3230033c61fb7394cf
mariusgg <marius.g.guei@gmail.com>
2019-01-13 02:27:19 +0000

    updates to readme and ran rubocop(linter)

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,18 +1,19 @@
-class GildedRose
+# frozen_string_literal: true
 
+class GildedRose
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name == "Aged Brie"
+      if item.name == 'Aged Brie'
         aged_brie(item)
-      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+      elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
         backstage_passes(item)
-      elsif item.name == "Sulfuras, Hand of Ragnaros"
+      elsif item.name == 'Sulfuras, Hand of Ragnaros'
         sulfuras(item)
-      elsif item.name == "Conjured"
+      elsif item.name == 'Conjured'
         conjured(item)
       else
         normal_item(item)
@@ -24,27 +25,24 @@ class GildedRose
     if item.quality == 0
       item.sell_in = item.sell_in - 1
     else item.quality < 50
-      item.quality = item.quality - 1
-      item.sell_in = item.sell_in - 1
-      if item.sell_in <= 0
-        item.quality = 0
-      end
+         item.quality = item.quality - 1
+         item.sell_in = item.sell_in - 1
+         item.quality = 0 if item.sell_in <= 0
     end
   end
+
   def aged_brie(item)
-    if item.sell_in > 0
-      item.quality +=  1
-    else
-      item.quality += 2
-    end
+    item.quality += if item.sell_in > 0
+                      1
+                    else
+                      2
+                    end
     item.sell_in -= 1
-    if item.quality > 50
-      item.quality = 50
-    end
+    item.quality = 50 if item.quality > 50
   end
-  #this seems to pass(2)test even though its empty
-  def sulfuras(item)
-  end
+
+  # this seems to pass(2)test even though its empty
+  def sulfuras(item); end
 
   def backstage_passes(item)
     item.quality += 1
@@ -53,5 +51,4 @@ class GildedRose
     item.quality = 0 if item.sell_in <= 0
     item.quality = 50 if item.quality > 50
   end
-
 end
